48e96c20c6ef0ddedfb0318835e7c56c2fe7444e
vgenovese <vaninageovese@gmail.com>
2024-02-03 22:29:10 -0300

    localhost funcionando

--- backend/main.go
+++ backend/main.go
@@ -7,19 +7,18 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "hola mundo desde el servidor go")
+	fmt.Println(w, "hola recibida en handler")
+	fmt.Fprint(w, "hola, desde servidor goland")
 }
 
 func main() {
 	// Configurar rutas
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/test", handler)
 
 	// servidor
-	port := 8080
-	fmt.Printf("Servidor escuchando en http://localhost:%d\n", port)
+	port := 8000
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-
 }
