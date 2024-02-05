11673fadeddee719d5de1691e0c516593ebb1f31
vgenovese <vaninageovese@gmail.com>
2024-02-05 12:48:54 -0300

    +division de componentes front,+agregar componentes al app.svelte, -implementacion de logica para el back con entrada del usuario, *problemas con async y el if

--- calculadora-app/src/App.svelte
+++ calculadora-app/src/App.svelte
@@ -1,30 +1,70 @@
 <script>
-	export let name;
+	
+	import BotonesNum from "./BotonesNum.svelte";
+    import Operaciones from "./Operaciones.svelte";
+    import Historial from "./Historial.svelte";
+    import './styles.css';
+	
+	let inputValue = '';
+	let error ='';
+
+	const handleButtonClick =  async (value) => {
+		if (value === '=') {
+      handleEquals();
+    } else if (value === 'C' || value === 'CE') {
+      inputValue = '';
+      error = '';
+    } else {
+      inputValue += value;
+    }
+   /*ERROR DE ESCRITURA DEL IF DENTRO DEL ASYNC NO SE PORQUE
+   {#if value === '='} 
+	handleButtonClick();		
+    {:else if value === 'C'}
+      inputValue = ''; 
+	  error = '';
+    {:else if value === 'CE'}
+      inputValue = '';
+	  error = '';
+    {:else}
+      inputValue += value; 
+    {/if}*/
+  };
+
+  const handleEquals = async () => {
+	try {
+      const response = await fetch(`http://localhost:8000/calculadora?valor=${inputValue}`);
+      const data = await response.json();
+      
+      if (data.resultado !== undefined) {
+        if (data.error) {
+          error = `Error al realizar la operación: ${data.error}`;
+        } else {
+          if (typeof data.resultado === 'number' && !isNaN(data.resultado) && isFinite(data.resultado)) {
+            inputValue = (data.resultado % 1 !== 0) ? data.resultado.toFixed(2) : data.resultado;
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error al procesar la respuesta:', error.message);
+      error = `Error al procesar la respuesta: ${error.message}`;
+    }
+
+  };
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- componente-->
+<div>
+	<input bind:value={inputValue} class="p-2 mb-4" />
+	<BotonesNum on:click={handleButtonClick} />
+	<div class="p-2 mt-4 bg-black text-white">
+		{#if error}
+		{error}
+		{:else}
+		 Resultado: {inputValue}
+		 {/if}
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	</div>
+  </div>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ calculadora-app/src/BotonesNum.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let onButtonClick; //manejo de clicks
+   
+    const handleButtonClick = (value) => {
+        onButtonClick(value);
+    }
+</script>
+
+<!-- Componentes-->
+<div class="grid grid-cols-3 gap-2">
+    {#each [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.', '=', 'C', 'CE'] as buttonValue}
+     <button on:click={() => handleButtonClick(buttonValue)} class="bg-blue-500 text-white p-2 rounded">
+      {buttonValue}
+     </button>
+    {/each}
+</div>
